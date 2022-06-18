12be365999b146681783945a72bc2e05041a95f5
Wamuyu <michellewachira25@gmail.com>
2022-06-18 16:34:22 +0300

    commit tree 5c3180387c1871c464bce7aed2962d0bcf3ee020

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    let changeLetters = /[\W_]/g;
+    let replaceChangeWord = word.toLowerCase().replace(changeLetters, '');
+    let reverseWord =
+        replaceChangeWord.split('').reverse('').join('');
+    if (reverseWord === replaceChangeWord) {
+        return true
+    } else return false
 }
 
+
+// Write your algorithm here
 /* 
   Add your pseudocode here
+Start
+input word
+if the word == drow(palindrome),
+then output =true
+else output =false
+return word, output
+stop
 */
 
 /*
   Add written explanation of your solution here
+  /W matches any non-word characters, removes all non-alphanumeric characters  and is equivalent to A-Z,a-z and 0-9
+RegExp - remove unwanted characters denoted by either [\W_]//G OR /[^A-Za-z0-9]/g;
+So we use that to remove all the characters that are not part of the format given
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
